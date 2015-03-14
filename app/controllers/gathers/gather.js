6a8b082e8623e8197f2445943fd0d8d03b03423a
Erik Eggers <em.eggers@gmail.com>
2015-03-14 00:09:24 -0400

    Ended up not using controller for index. Gather images centered in thumbnails

--- app/controllers/gathers/gather.js
+++ app/controllers/gathers/gather.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   imageStyle: function() {
-    return "background: url('" + this.get('model.gatherImage.url') + "') center center;";
+    return "background: url('" + this.get('model.gatherImage.url') + "') center center no-repeat;";
   }.property('model.gatherImage.url')
 });
