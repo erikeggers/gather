5eb68c5f6a13a68cc3e5931fe2da3d065033bf9a
Erik Eggers <em.eggers@gmail.com>
2015-03-18 20:12:59 -0400

    update

--- app/routes/gathers/edit.js
+++ app/routes/gathers/edit.js
@@ -1,4 +1,12 @@
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  actions: {
+    createGather: function(){
+      this.modelFor('gathers.edit').save().then(function() {
+        this.transitionTo('gathers.index');
+      }.bind(this));
+    }
+  }
 });
