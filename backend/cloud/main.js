08f671b58ce993e42ae1382783e02c27541bb0e7
Erik Eggers <em.eggers@gmail.com>
2015-03-26 20:13:29 -0400

    added ability to see gatherings that an user is attending

--- backend/cloud/main.js
+++ backend/cloud/main.js
@@ -1,3 +1,4 @@
+/* global Parse */
 Parse.Cloud.define('search', function(request, response){
   var nameQuery = new Parse.Query("gatherings");
   nameQuery.matches("gatherName", new RegExp(request.params.search, 'i'));
@@ -9,6 +10,17 @@ Parse.Cloud.define('search', function(request, response){
   stateQuery.matches("gatherState", new RegExp(request.params.search, 'i'));
 
   var query = Parse.Query.or(nameQuery, cityQuery, stateQuery);
+  query.find().then(function(results) {
+    response.success(results);
+  }, function(error) {
+    response.error(error);
+  });
+});
+
+
+Parse.Cloud.define('attending', function(request, response){
+  var query = new Parse.Query("gatherings");
+  query.equalTo('attendees', request.params.user_id);
   query.find().then(function(results) {
     response.success(results);
   }, function(error) {
